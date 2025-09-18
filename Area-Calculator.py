9a4aa9bc7261bbfc95b64a85a09c644431af89e1
Evian <evianescobio@outlook.com>
2025-09-18 12:29:15 -0400

    Update Area-Calculator.py

--- Area-Calculator.py
+++ Area-Calculator.py
@@ -1,17 +1,30 @@
-# This is the heading of the program that prints the options we want to calculate.
+import math
+# This is the heading of the program.
 print("==================")
 print("Area Calculator 📐")
-print("==================")
-print("")
+print("==================\n")
 
-print("1) Triangle")
-print("2) Rectangle")
-print("3) Square")
-print("4) Circle")
-print("5) Quit")
-print("")
+# Display the menu.
+while True: 
+    print("1) Triangle")
+    print("2) Rectangle")
+    print("3) Square")
+    print("4) Circle")
+    print("5) Quit\n")
+    
+    # Ask the user to choose the shape.
+    choice = int(input("Which shape: "))
 
-# Ask the user for a shape to calculate its area.
-print(int(input("Which shape: ")))
-print("")
+    # Calculate and display the area of the rectangle.
+    if choice == 1:
+        base = int(input("Base: "))
+        height = int(input("Height: "))
+        area = (base * height) / 2
+        print(f"The area of the triangle is {area}\n")
 
+    # Calculate and display the area of the rectangle.
+    elif choice == 2:
+          base = int(input("Base: ")
+          height = int(input("Height: "))
+          area = base * height
+          print(f"The area of the circle is {area}\n")
