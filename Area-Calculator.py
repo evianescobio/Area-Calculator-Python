6b53faa5e4ebada26b9f0dbba6f3858bce28c609
Evian <evianescobio@outlook.com>
2025-09-18 11:44:53 -0400

    Area-Calculator.py

--- Area-Calculator.py
+++ Area-Calculator.py
@@ -1,3 +1,17 @@
+# This is the heading of the program that prints the options we want to calculate.
 print("==================")
 print("Area Calculator 📐")
 print("==================")
+print("")
+
+print("1) Triangle")
+print("2) Rectangle")
+print("3) Square")
+print("4) Circle")
+print("5) Quit")
+print("")
+
+# Ask the user for a shape to calculate its area.
+print(int(input("Which shape: ")))
+print("")
+
